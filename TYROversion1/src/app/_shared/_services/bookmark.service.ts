cf4f420a0a4a8477dc80bc7c922df32dd2eb65a2
MohitPaliwal12 <mohitpaliwal@infrrd.ai>
2020-12-04 16:47:20 +0530

    changes in bookmark for setting  ProfilePicture

--- TYROversion1/src/app/_shared/_services/bookmark.service.ts
+++ TYROversion1/src/app/_shared/_services/bookmark.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 //import { BehaviorSubject, Observable } from 'rxjs';
 //import { Observable } from 'rxjs/internal/Observable';
@@ -31,4 +31,12 @@ export class BookmarkService {
     const postData = { "articleId" : id}
     return this.http.post<any>(`${environment.apiUrl}/posts/removeBookmark`,postData);
   }
+
+  getProfilePicture(id)
+  {
+    let params = new HttpParams().set("id",id);
+    return this.http.get<any>(`${environment.apiUrl}/profile/getProfilePicture`,{params:params});
+
+  }
+
 }
