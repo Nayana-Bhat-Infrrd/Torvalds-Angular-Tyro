4d00ddc03babbc33d7878bb07b71b21fd4984fbb
MohitPaliwal12 <mohitpaliwal@infrrd.ai>
2020-12-04 19:04:16 +0530

    profilePicture updations in bookmark and header

--- TYROversion1/src/app/_shared/title/title.component.ts
+++ TYROversion1/src/app/_shared/title/title.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
-
+import { data } from 'jquery';
+import { DashboardService } from 'src/app/_shared/_services/dashboard.service';
 
 @Component({
   selector: 'app-title',
@@ -13,14 +14,30 @@ export class TitleComponent implements OnInit, OnChanges {
 
   @Input() buttonName: string;
   @Input() disableButton: boolean;
-
-  constructor() {
+  public profilePicture;
+  constructor(private dashboardService:DashboardService) {
 
   }
 
+
+  
+
+
+
+
   ngOnInit(): void {
     console.log("disableButton : " + this.disableButton);
-    
+    this.dashboardService.getProfile().subscribe(
+       data=>{
+            console.log("Profile from title:"+data.profilePictureUrl);
+            this.profilePicture = data.profilePictureUrl;
+       },
+       error=>
+       {
+                console.log("error");
+       }
+
+    )
 
   }
 
